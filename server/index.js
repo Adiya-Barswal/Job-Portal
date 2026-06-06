3c3116a7e7171cd4f394c1508d24f7ba06fb61cb
Aditya Singh <adityasinghbarswal7@gmail.com>
2026-06-06 12:04:52 +0530

    full project

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import  dotenv  from "dotenv";
+import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
-import userRoute from "./routes/user.route.js"
-import companyRoute from "./routes/company.route.js"
-import jobRoute from "./routes/job.route.js"
-import  applicationRoute from "./routes/application.route.js";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
 dotenv.config({});
 
 const app = express();
@@ -16,34 +16,33 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// cors
 const corsOptions = {
-  origin: [ "http://localhost:5173"],
+  origin: process.env.CLIENT_URL,
   credentials: true,
 };
 
 app.use(cors(corsOptions));
 
+const PORT = process.env.PORT || 3000;
 
-const PORT =  process.env.PORT ||3000;
+//Api's(user, company, job)
 
-//Api's    (user, company, job)
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
 
-app.use("/api/v1/user",userRoute);
-app.use("/api/v1/company",companyRoute);
-app.use("/api/v1/job",jobRoute);
-app.use("/api/v1/application",applicationRoute);
-
-
-// for example  
+// for example
 
 //   "http://localhost:8000/api/v1/user/register"
 //   "http://localhost:8000/api/v1/user/login"
 //   "http://localhost:8000/api/v1/user/logout"
 //   "http://localhost:8000/api/v1/user/profile/update"
 
-
+// Start Server
 connectDB().then(() => {
-app.listen(PORT, () => {
-  console.log(`Server running at port ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`);
+  });
 });
