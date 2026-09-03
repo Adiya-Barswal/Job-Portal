917a43e1e99eddd6c9633222fa8d6fbf68b6e949
Aditya Singh <adityasinghbarswal7@gmail.com>
2026-09-03 14:48:07 +0530

    Fix MongoDB URI format and update db connection

--- server/utils/db.js
+++ server/utils/db.js
@@ -2,11 +2,15 @@ import mongoose from "mongoose";
 import dns from "dns";
 
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
+dns.setDefaultResultOrder("ipv4first");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    family: (4, console.log("mongodb connected successfully"));
+    await mongoose.connect(process.env.MONGO_URI, {
+      family: 4,
+      serverSelectionTimeoutMS: 5000,
+    });
+    console.log("MongoDB connected successfully");
   } catch (error) {
     console.log("MongoDB connection error:", error);
     process.exit(1);
